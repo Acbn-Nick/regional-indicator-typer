fd305618d3ef217f08cc4280fdcd81779950e7bc
Acbn-Nick <acbn.pwbk@gmail.com>
2020-09-17 15:21:55 -0500

    Added favicon

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/Acbn-Nick/regional-indicator-typer
 go 1.14
 
 require (
+	github.com/getlantern/systray v1.0.4
 	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.3.0 // indirect
--- internal/client.go
+++ internal/client.go
@@ -1,21 +1,42 @@
 package client
 
 import (
+	"io/ioutil"
+	"time"
+
+	"github.com/getlantern/systray"
 	log "github.com/sirupsen/logrus"
 )
 
-type client struct {
+type Client struct {
+	conf *Config
 }
 
-func New() *client {
-	return &client{}
+func New() *Client {
+	c := NewConfig()
+
+	if err := c.loadConfig(); err != nil {
+		log.Infof("failed to load config")
+	}
+
+	return &Client{conf: c}
 }
 
-func (c *client) Start() {
+func (c *Client) Start() {
 	log.Info("starting client")
 
+	go c.tray()
+
 }
 
-func (c *client) tray() {
-
+func (c *Client) tray() {
+	ico, err := ioutil.ReadFile("../assets/favicon.ico")
+	if err != nil {
+		log.Fatal("error loading systray icon ", err.Error())
+	}
+	time.Sleep(500 * time.Millisecond) // Add 500ms delay to fix issue with systray.AddMenuItem() in goroutines on Windows.
+	systray.SetIcon(ico)
+	systray.SetTitle("Regional Indicator Typer")
+	systray.SetTooltip("Regional Indicator Typer")
+	quit := systray.AddMenuItem("Quit", "Quit")
 }
